9257e3ad60e1104108af41010f2b7b673502d32b
Howon Lee <howon@howonlee.com>
2015-10-01 12:41:11 -0700

    add the outlines of the code

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 Constructor.IO client for PHP
 ====
 
-
+This is an object-oriented PHP library for using Constructor.IO, the next generation of autocomplete. Ain't it spiffy?
---
+++ src/ConstructorIO.php
@@ -0,0 +1,24 @@
+<?php
+class ConstructorIO {
+  // actually, just make them private
+  function _initialization or something;
+
+  function _serializeParams;
+
+  function _makeUrl;
+
+  function query;
+
+  function add;
+
+  function remove;
+  
+  function modify;
+
+  function track_conversion;
+  
+  function track_click_through;
+  
+  function track_search;
+}
+?>
